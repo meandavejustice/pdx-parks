1502295d1b091ab184111724a2d722305a809df0
David Adam Justice <david@outthinkgroup.com>
2014-04-04 10:19:05 -0700

    fix deprecated mq api and add keepalive

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var request = require('request');
 var logfmt = require('logfmt');
+var keepAlive = require('keep-alive')('http://pdx-parks.org.com', 1800000);
 var deparam = require('node-jquery-deparam');
 var port = Number(process.env.PORT || 3000);
 var app = express();
@@ -10,7 +11,7 @@ app.use('/public', express.static(__dirname + '/public'));
 app.use(express.favicon(__dirname + 'public/images/favicon.ico'));
 
 app.get('/', function(req, res){
-  res.sendfile(__dirname + '/index.html')
+  res.sendfile(__dirname + '/index.html');
 });
 
 app.get('/parks', function(req, res){
